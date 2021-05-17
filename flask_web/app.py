e80befabb179b42ab79b5ce7d42a10345294147b
Prakhar Agarwal <prackode@gmail.com>
2021-05-17 22:26:51 +0530

    Login page updated

--- flask_web/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-
-@app.route("/",methods=["POST","GET"])
-def login():
-    return render_template("login.html")
-
-if __name__=="__main__":
-    app.run(debug=True)
